14c793bca8c275f66a3020c32175464c347b63c3
ujwol <dahalujwol75@gmail.com>
2024-12-25 14:07:32 -0600

    updated

--- build.ps1
+++ build.ps1
@@ -1,35 +1,6 @@
 param (
     [string]$rule = "default"
 )
-<# NOTE: Update these variables to target different files with this script. #>
-$MAIN = "src.app"
-# ======================================================================== #
-$CP_DELIM = ";"
-
-# Define the source and destination directories
-$sourceDir = "C:\projects-v2\udahal2.github.io\"
-$destinationDir = "C:\projects-v2\udahal2.github.io\build\"
-
-# Create the destination directory if it doesn't exist
-if (-Not (Test-Path -Path $destinationDir)) {
-    New-Item -ItemType Directory -Path $destinationDir
-}
-
-# Copy all files except about.md
-Get-ChildItem -Path $sourceDir -Recurse | Where-Object {
-    $_.FullName -notlike "*about.md"
-} | ForEach-Object {
-    $destinationPath = $_.FullName.Replace($sourceDir, $destinationDir)
-    $destinationDirPath = [System.IO.Path]::GetDirectoryName($destinationPath)
-    if (-Not (Test-Path -Path $destinationDirPath)) {
-        New-Item -ItemType Directory -Path $destinationDirPath
-    }
-    Copy-Item -Path $_.FullName -Destination $destinationPath
-}
-
-Write-Output "Build completed. about.md is excluded from the build."
-
-
 
 if ($IsMacOS -or $IsLinux) {
     $CP_DELIM = ":" # changes to : for Mac or Linux
